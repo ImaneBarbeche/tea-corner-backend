e48cbb12609934d1920d1f911b72f575bba36b19
GeorgeDaris <george.darisgr@gmail.com>
2026-02-02 11:48:41 +0100

    fix: updated db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // ← VERY IMPORTANT
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '5432'),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      database: process.env.DB_NAME,
       entities: [],
       synchronize: true,
       autoLoadEntities: true,
