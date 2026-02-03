7d6664746280a90af77f0aadced5871755505491
Imane <barbecheimane@gmail.com>
2026-02-03 11:41:50 +0100

    feature: installed argon2 package for password hashing and usage inside auth service for the signup function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { CreateUserDto } from 'src/user/create-user.dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -9,22 +10,25 @@ export class AuthService {
     // private jwtService: JwtService,
   ) {}
 
-  async signIn(username: string, password: string): Promise<any> {
-    const user = await this.userService.findOne(username);
-    if (user?.password != password) {
-      throw new UnauthorizedException();
-    }
-    const { password, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
-  }
+//   async signIn(username: string, password: string): Promise<any> {
+//     const user = await this.userService.findOne(username);
+//     if (user?.password != password) {
+//       throw new UnauthorizedException();
+//     }
+//     const { password, ...result } = user;
+//     // TODO: Generate a JWT and return it here
+//     // instead of the user object
+//     return result;
+//   }
 
-  
+
   async signUp(createUserDto: CreateUserDto) {
+    // Hasher avec Argon2
+    const hashedPassword = await argon2.hash(createUserDto.password);
+
     const data = {
       ...createUserDto,
-      // password: hashPass,
+      password: hashedPassword,
     };
     return this.userService.create(data);
   }
