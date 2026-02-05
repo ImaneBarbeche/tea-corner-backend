e8ee27145cd936eb7edde7fe98e419570b62328a
Imane <barbecheimane@gmail.com>
2026-02-05 13:18:40 +0100

    feature: enabled cors and installed cookie parser and csrf packages

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // react url
+    credentials: true, // cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT ?? 3000);
 }
