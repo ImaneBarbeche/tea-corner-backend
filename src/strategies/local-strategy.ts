6d01694d8440dad7eb09fb2ab5f4ca3c043b1dbc
Imane <barbecheimane@gmail.com>
2026-02-16 22:06:26 +0100

    fix: sign in error because local strategy did not recognize user_name

--- src/strategies/local-strategy.ts
+++ src/strategies/local-strategy.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../auth/auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    super({ usernameField: 'user_name' }); // Indique à Passport d'utiliser `user_name`
   }
 
   async validate(username: string, password: string): Promise<any> {
