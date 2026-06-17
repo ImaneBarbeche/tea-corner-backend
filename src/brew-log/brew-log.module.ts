1c05057ee6d7577b04a4fad2e03d217f65c461b7
GeorgeDaris <george.darisgr@gmail.com>
2026-06-17 17:47:43 +0200

    fix: added brew log module imports and exports

--- src/brew-log/brew-log.module.ts
+++ src/brew-log/brew-log.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BrewLogController } from './brew-log.controller';
 import { BrewLogService } from './brew-log.service';
+import { BrewLog } from './brew-log.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BrewLog])],
   controllers: [BrewLogController],
-  providers: [BrewLogService]
+  providers: [BrewLogService],
+  exports: [TypeOrmModule],
 })
 export class BrewLogModule {}
